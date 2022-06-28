ae16539d38d1a04675670fd0f57c1d8430c75150
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-06-27 22:09:14 -0500

    estructurando login--css puro

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserModule} from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -18,8 +16,9 @@ import { StateOrdersComponent } from './components/state-orders/state-orders.com
     StateOrdersComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    AppRoutingModule,
+    BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
