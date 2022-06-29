4f51f131bc06f9c71914169dd7647c2d865e42a1
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-06-29 17:34:19 -0500

    obteniendo valores del formulario

--- burguer-queen/src/app/components/login/login.component.ts
+++ burguer-queen/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
 
-
+  login(form:NgForm){
+    const email =  form.value.email;
+    const password = form.value.password;
+  console.log(email + "------" + password)
+  }
 
 }
