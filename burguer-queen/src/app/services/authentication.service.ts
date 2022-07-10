c56612b4fc44c8f5923a147ea27337aa69c3973b
Carol Chero Paz <cheropazpilar@gmail.com>
2022-07-09 22:46:49 -0500

    cambios en rest.service

--- burguer-queen/src/app/services/authentication.service.ts
+++ burguer-queen/src/app/services/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { User } from '../models/user.model.data';
+//import { User } from '../models/user.model.data';
 import { BehaviorSubject, catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
