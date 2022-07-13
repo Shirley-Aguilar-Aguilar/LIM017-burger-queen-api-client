7b2ed8974ef5a5984a78c69bc97f80a7b978b1a7
Carol Chero Paz <cheropazpilar@gmail.com>
2022-07-13 11:58:34 -0500

    Fix: states orders

--- burguer-queen/src/app/components/state-orders/state-orders.component.ts
+++ burguer-queen/src/app/components/state-orders/state-orders.component.ts
@@ -18,27 +18,3 @@ export class StateOrdersComponent implements OnInit {
 }
 
 
-
-
-import { OrdersFirebaseService } from 'src/app/services/orders-firebase.service';
-
-@Component({
-  selector: 'app-list-orders',
-  templateUrl: './list-orders.component.html',
-  styleUrls: ['./list-orders.component.scss']
-})
-
-export class ListOrdersComponent implements OnInit {
-  orders: any[];
-  constructor(private orderFirebase : OrdersFirebaseService) {this.orders=[{}]}
-
-  ngOnInit(): void {
-    this.orderFirebase.getOrders().subscribe(orders => {
-      this.orders = orders;
-    })
-  }
-
-  changeStatus(index:number, status: string): Promise<any>{
-    return this.orderFirebase.updateStatusOrder(this.orders[index],status);
-  }
-}
